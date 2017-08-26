e75489cdb7da4a37c639a832f5f087d58bfddbd8
lingtalfi <lingtalfi@gmail.com>
2017-08-26 13:32:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,245 @@
+MyAutoComplete
+===================
+2017-08-25
+
+
+
+A jquery autocomplete plugin.
+
+
+
+
+Features
+=============
+- lightweight (the commented version is about 400 lines)
+- code easy to understand
+- is rendering agnostic 
+
+
+
+
+How does it work?
+===================
+
+The auto-complete session starts when the user types something into an input.
+Then data is fetched from somewhere.
+Then the fetched data is rendered.
+
+The code is organized in two parts:
+
+- fetching and rendering the data  
+- interacting with the rendered data
+
+
+
+Fetching and rendering the data
+==============================
+
+The fetching starts when an alpha-numeric-punct char is typed on
+the target input.
+In the current implementation, the fetching part uses an ajax service and/or a cache.
+
+In future implementation, other ways of fetching data can be added.
+
+The ajax service can return anything, which is called data.
+
+The data is then passed to the render functions which display the data how you want it. 
+
+So, you could display a simple list of text items, or a more complex multiple search system
+with different sections and images, it's up to you.
+
+
+Nomenclature
+------------
+
+Nomenclature is important:
+
+- query: is the string typed by the user: the value of the target input at any given time
+- data: this is the result of the ajax service 
+
+
+
+
+Interacting with the rendered data
+==================================
+
+This phase is about handling the following problems:
+
+- what happens if the user selects an item
+- what happens if the user blurs out of the target input
+
+
+The myAutoComplete plugin is agnostic, but provides some tools to help you implement
+common gui logic (like for instance an up/down arrow navigation system).
+
+
+All this is explained in the source's comments.
+
+
+  
+Example
+============
+
+I have no time for a simple example, but here is the example I've just finished today.
+
+This is a complex example because the search engine yields two types of results at the same time:
+
+- products
+- categories
+
+(this is a search engine for e-commerce)
+
+A list of images appear for products, while the category list contains only simple text items.
+The result on the development website looks like this:
+
+
+[![myautocomplete.png](https://s19.postimg.org/83xaog9cz/myautocomplete.png)](https://postimg.org/image/qjhrlunhb/)
+
+So here is the html code:
+
+```html
+           <div class="searchbar-container">
+                <div class="searchbar">
+                    <div class="search-icon lee-icon" id="topmenu-searchtrigger-small"></div>
+                    <form method="post"
+                          action="">
+                        <input type="text" placeholder="Chercher" id="searchbar-input">
+                    </form>
+                    <button class="cancel-search lee-icon" id="searchbar-cancel">Cancel</button>
+                    <ul>
+                        <li class="topmenu-action-search"><span class="lee-icon action action-search">Recherche</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search-results-panel">
+                    <div class="section section-categories">
+                        <div class="title">SUGGESTION DE CATÉGORIES</div>
+                        <ul class="items-list-categories"></ul>
+                    </div>
+                    <div class="section section-products">
+                        <div class="title">SUGGESTION DE PRODUITS</div>
+                        <ul class="items-list"></ul>
+                    </div>
+                    <div class="templates" style="display: none">
+                        <ul class="items-list">
+                            <li class="item" data-uri="{-uri_card-}">
+                                <div class="image">
+                                    <img data-src="{-uri_thumb-}"
+                                         alt="{-%label-}">
+                                </div>
+                                <span class="label">{-label-}</span>
+                                <span class="attributes">{-attr_string-}</span>
+                                <span class="ref">ref. {-ref-}</span>
+                                <span class="price">
+                                        <span class="price-text">{-sale_price-}</span>
+                                        <abbr class="price-type">{-price_type-}</abbr></span>
+                            </li>
+                        </ul>
+                        <ul class="items-list-categories">
+                            <li class="item" data-uri="{-uriCategory-}"><a href="{-uriCategory-}">{-label-}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+```
+
+
+Note: you may have noticed the ".templates" div, in which I put the templates used 
+to feed the results panel. 
+By the way, in this code I'm using the [cloneTemplate](https://github.com/lingtalfi/cloneTemplate) technique to
+inject items into the search results panel.
+
+
+And here is the jquery code:
+
+```js
+    var jTopBar = $('#site-topbar');
+    var jSearchContainer = $('.searchbar-container', jTopBar);
+    var jSearchResultsPanel = $('.search-results-panel', jSearchContainer);
+    var jSearchInput = $('#searchbar-input');
+    var jSearchCategorySection = jSearchContainer.find('.section-categories');
+    var jSearchProductsSection = jSearchContainer.find('.section-products');
+    var jSearchTemplates = jSearchContainer.find('.templates');
+    var jSearchProductTpl = jSearchTemplates.find('.items-list .item');
+    var jSearchCategoryTpl = jSearchTemplates.find('.items-list-categories .item');
+
+
+    function refreshSearchPanel(data, query) {
+        //----------------------------------------
+        // PRODUCTS
+        //----------------------------------------
+        var jUl = jSearchProductsSection.find('.items-list');
+        jUl.empty();
+        var jClone = null;
+        var products = data.products;
+        if ($.isEmptyObject(products)) {
+            jSearchProductsSection.hide();
+        }
+        else {
+            jSearchProductsSection.show();
+        }
+        for (var i in products) {
+            var product = products[i];
+            jClone = $.fn.cloneTemplate(jSearchProductTpl, product);
+            jUl.append(jClone);
+        }
+
+
+        //----------------------------------------
+        // CATS
+        //----------------------------------------
+        var jUl2 = jSearchCategorySection.find('.items-list-categories');
+        jUl2.empty();
+        var cats = data.categories;
+        if ($.isEmptyObject(cats)) {
+            jSearchCategorySection.hide();
+        }
+        else {
+            jSearchCategorySection.show();
+        }
+        for (var i in cats) {
+            var cat = cats[i];
+            jClone = $.fn.cloneTemplate(jSearchCategoryTpl, cat);
+            jUl2.append(jClone);
+        }
+    }
+
+
+    jSearchInput.myAutoComplete({
+        uriService: "/service/Ekom/json/api?action=product-search",
+        renderData: function (data, query) {
+            if (null !== data) {
+                refreshSearchPanel(data, query);
+            }
+            else {
+                var jUl = jSearchProductsSection.find('.items-list');
+                jUl.empty();
+                var jUl2 = jSearchCategorySection.find('.items-list-categories');
+                jUl2.empty();
+            }
+        },
+        minChars: 3,
+        panel: jSearchResultsPanel,
+        isEmptyResults: function (data) {
+            return (
+                true === $.fn.myAutoComplete.isEmpty(data.products) &&
+                true === $.fn.myAutoComplete.isEmpty(data.categories)
+            );
+        }
+    });
+
+``` 
+
+ 
+Hopefully this example helps you understand how the plugin works.
+ 
+
+
+
+
+
+
+ 
+ 
---
+++ myautocomplete.js
@@ -0,0 +1,412 @@
+(function ($) {
+
+
+
+
+    // https://davidwalsh.name/javascript-debounce-function
+    var debounce = function (func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+
+    $.myAutoComplete = function (element, options) {
+
+
+        var cache = {};
+
+        var jInput = $(element);
+        var o = $.extend({}, $.fn.myAutoComplete.defaults, options);
+        var jPanel = o.panel;
+        var serviceUri = o.uriService;
+        var hasQuestionMark = (-1 !== serviceUri.indexOf('?'));
+        var curItemClass = o.currentItemClass;
+
+
+        //----------------------------------------
+        // FETCH AND RENDER
+        //----------------------------------------
+        /**
+         * Fetch the data (either from cache or from ajax service),
+         * and call the onSuccess (or onError) callback in case of success (or error).
+         *
+         * @param query
+         * @param onSuccess
+         * @param onError
+         */
+        function fetchData(query, onSuccess, onError) {
+            // use cache?
+            if (true === o.useCache) {
+                if (query in cache) {
+                    return onSuccess(cache[query]);
+                }
+            }
+
+            // default ajax fetching
+            var uri = serviceUri;
+            if (true === hasQuestionMark) {
+                uri += '&';
+            }
+            else {
+                uri += '?';
+            }
+            uri += 'query=' + query;
+
+            var data = {};
+            var jxqhr = $.post(uri, data, function (data) {
+                if (true === o.useCache) { // put result in cache for later re-use
+                    cache[query] = data;
+                }
+                onSuccess(data);
+            }, "json").fail(function () {
+                onError && onError(jxqhr);
+            });
+
+        }
+
+
+        function getCurrentItemIndex() {
+            var jCurrent = jPanel.find('.' + curItemClass);
+            if (jCurrent.length) {
+                var index = jCurrent.index("." + o.itemClass);
+            }
+            else {
+                index = -1;
+            }
+            return index;
+        }
+
+        function adjustScroll(jItem) {
+            var $container = jPanel;
+            var $scrollTo = jItem;
+
+            $container.animate({
+                scrollTop: $scrollTo.offset().top - $container.offset().top + $container.scrollTop(),
+                scrollLeft: 0
+            }, 100);
+        }
+
+        function navigatePrevItem() {
+            var index = getCurrentItemIndex();
+            index--;
+            jPanel.find("." + curItemClass).removeClass(curItemClass);
+            jPanel.find("." + o.itemClass).eq(index).addClass(curItemClass);
+            adjustScroll(jPanel.find("." + curItemClass));
+        }
+
+        function navigateNextItem() {
+            var index = getCurrentItemIndex();
+            index++;
+            jPanel.find("." + curItemClass).removeClass(curItemClass);
+            jPanel.find("." + o.itemClass).eq(index).addClass(curItemClass);
+            adjustScroll(jPanel.find("." + curItemClass));
+        }
+
+
+        function showPanel(jPanel) {
+            if (true === o.useKeyboardNav && null !== o.itemClass) {
+
+                $(document).off("keyup.myautocomplete").on("keyup.myautocomplete", function (e) {
+                    if (
+                        38 === e.which || // up
+                        40 === e.which  // down
+                    ) {
+                        if (40 === e.which) {
+                            navigateNextItem();
+                        }
+                        else {
+                            navigatePrevItem();
+                        }
+                    }
+
+                    if (
+                        13 === e.which &&
+                        true === o.useEnterToClickCurrentItem
+                    ) {
+                        var jCur = jPanel.find("." + curItemClass);
+                        if (jCur.length) {
+                            o.onSelect(jCur);
+                            return false;
+                        }
+                    }
+
+                });
+            }
+            o.showPanel(jPanel);
+        }
+
+        function hidePanel(jPanel) {
+            $(document).off("keyup.myautocomplete");
+            o.hidePanel(jPanel);
+        }
+
+
+        jInput.on("keyup.myautocomplete", debounce(function (e) {
+            /**
+             * We only trigger fetch data on  alpha num punct chars,
+             * plus the following chars, which should update the query also:
+             *
+             * - backspace
+             * - delete
+             *
+             * https://css-tricks.com/snippets/javascript/javascript-keycodes/
+             */
+            if (
+                8 === e.which || // backspace
+                46 === e.which || // delete
+                (e.which >= 48 && e.which <= 90) || // 0-9 and a-z
+                (e.which >= 96 && e.which <= 105) || // numpad 0-9
+                (e.which >= 186 && e.which <= 192) || // various punctuations
+                (e.which >= 219 && e.which <= 222) // various punctuations
+            ) {
+                var query = $(this).val();
+                var strlen = query.length;
+                if (strlen >= o.minChars) {
+
+                    fetchData(query, function (data) {
+
+
+                        if (jPanel) { // use panel goodies?
+                            if (o.isEmptyResults(data)) {
+                                hidePanel(jPanel);
+                            }
+                            else {
+                                showPanel(jPanel);
+                            }
+                        }
+
+
+                        o.renderData(data, query);
+                    }, o.onAjaxFail);
+                }
+                else {
+                    if (jPanel) { // use panel goodies?
+                        hidePanel(jPanel);
+                    }
+                    o.renderData(null, query);
+                }
+            }
+
+        }, o.delay));
+
+
+        jInput.on("blur.myautocomplete", function (e) {
+            if (jPanel) { // use panel goodies?
+                setTimeout(function () {
+                    hidePanel(jPanel);
+                }, 200);
+            }
+        });
+
+
+        //----------------------------------------
+        // GUI INTERACTION WITH RESULTS
+        //----------------------------------------
+        if (jPanel) {
+            if (null !== o.itemClass) {
+                jPanel.on('click', function (e) {
+                    var jTarget = $(e.target);
+                    var jItem = jTarget.closest("." + o.itemClass);
+                    if (jItem.length) {
+                        o.onSelect(jItem);
+                        return false;
+                    }
+                });
+            }
+        }
+
+
+    };
+
+    $.fn.myAutoComplete = function (options) {
+        return this.each(function () {
+            if (undefined == $(this).data('myAutoComplete')) {
+                var plugin = new $.myAutoComplete(this, options);
+                $(this).data('myAutoComplete', plugin);
+            }
+        });
+    };
+
+
+    //----------------------------------------
+    // UTILS
+    //----------------------------------------
+    /**
+     * Create space separated components from query.
+     * For each component, search/replace by bold version in resultLabel.
+     */
+    $.fn.myAutoComplete.decorateLabel = function (query, resultLabel) {
+        query = query.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        var re = new RegExp("(" + query.split(' ').join('|') + ")", "gi");
+        return resultLabel.replace(re, "<b>$1</b>");
+    };
+
+    /**
+     * If $something is an array or an object:
+     *      return true if empty, false otherwise
+     * If $something is not an array nor an object:
+     *      returns $theDefault=true
+     */
+    $.fn.myAutoComplete.isEmpty = function (something, theDefault) {
+        if (jQuery.isArray(something) || jQuery.isPlainObject(something)) {
+            if (
+                (jQuery.isArray(something) && 0 === something.length) ||
+                jQuery.isEmptyObject(something)
+            ) {
+                return true;
+            }
+            return false;
+        }
+        if ('undefined' === typeof theDefault) {
+            theDefault = true;
+        }
+        return theDefault;
+    };
+
+
+    //----------------------------------------
+    // DEFAULTS
+    //----------------------------------------
+    $.fn.myAutoComplete.defaults = {
+        //----------------------------------------
+        // AJAX COMMUNICATION - FETCHING AND RENDERING
+        //----------------------------------------
+        useCache: true,
+        delay: 150,
+        minChars: 1,
+        uriService: "/service/Ekom/json/api?action=product-search",
+        onAjaxFail: function (jxqhr) {
+            console.log("myautocomplete: onAjaxFail with error: " + jxqhr.responseText);
+        },
+        /**
+         * If data is null, it's the signal that the panel should
+         * be empty.
+         */
+        renderData: function (data, query) {
+            console.log("render data");
+            console.log(data, query);
+        },
+        //----------------------------------------
+        // PANEL GUI INTERACTION
+        //----------------------------------------
+        /**
+         * The rendering panel: a jquery element like $("#panel").
+         * If null (default), you are on your own.
+         * If not null, the myAutoComplete plugin tries to
+         * help you with various methods.
+         *
+         */
+        panel: null,
+        /**
+         * only if panel is not null.
+         *
+         * After a stroke in the search input, examine the length
+         * of the query string. If greater than minChars, show
+         * the panel, otherwise hide the panel.
+         *
+         * Also, if the results data is empty the panel will hide (and show if not empty).
+         * Whether or not the results data is empty is defined by the isEmptyResults option.
+         *
+         * Also, when the user focus out of the search input, the panel will hide.
+         *
+         */
+        usePanelShowHide: true,
+        /**
+         * The method used to show the panel
+         */
+        showPanel: function (jPanel) {
+            jPanel.addClass("visible");
+        },
+        /**
+         * The method used to hide the panel
+         */
+        hidePanel: function (jPanel) {
+            jPanel.removeClass("visible");
+        },
+        /**
+         * Function to evaluate whether or not the returned data
+         * is empty.
+         *
+         * By default, if the data is an non empty array or non empty object
+         * returns true. In all other case return false.
+         */
+        isEmptyResults: function (data) {
+            if (jQuery.isArray(data) || jQuery.isPlainObject(data)) {
+                if (
+                    (jQuery.isArray(data) && 0 === data.length) ||
+                    jQuery.isEmptyObject(data)
+                ) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        },
+        /**
+         * The panel appears, but then what happens if the user clicks
+         * on an item?
+         * This plugin can help you with this by providing a click handler
+         * on items.
+         *
+         * Each query yields a number of items that you display in the panel.
+         *
+         * If your query has the class defined by this option (itemClass), then a click on
+         * it will trigger the onSelect action.
+         *
+         * If itemClass is null, this behavior is not used, and you handle the click
+         * behaviour for yourself.
+         *
+         * The $itemClass is searched in the context of the panel,
+         * and therefore the panel must be defined too.
+         *
+         */
+        itemClass: 'item',
+        /**
+         * if you use the "itemClass" behaviour,
+         * then what happens when the user clicks an item?
+         *
+         * By default, it redirects to the uri defined on the
+         * item's data-uri attribute
+         */
+        onSelect: function (jItem) {
+            var href = jItem.attr('data-uri');
+            location.href = href;
+        },
+        /**
+         * If panel is defined AND itemClass is defined only
+         * Will allow the user to navigate through items (with class $itemClass)
+         * using up/down arrows
+         */
+        useKeyboardNav: true,
+        /**
+         * When keyboard navigation is on,
+         * the item with focus gets the $currentItemClass class.
+         */
+        currentItemClass: "active",
+        /**
+         * Only if the following options are set:
+         *      - panel
+         *      - useKeyboardNav
+         *      - itemClass
+         *      - currentItemClass
+         *
+         * If true,
+         *      if enter is pressed AND an active item is found,
+         *      then the onSelect method (option) is called on the item.
+         */
+        useEnterToClickCurrentItem: true
+    };
+
+
+})(jQuery);
+
